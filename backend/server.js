5f99ee530856c834745f903388caf834b9abcd45
hossein abbasifar <hossein.abbasifar@gmail.com>
2025-03-24 21:04:09 +0330

    MongoDB connected

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,17 @@
 import express from "express";
-import products from "./data/products.js";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 
-const port = process.env.PORT || 5000; 
+const port = process.env.PORT || 5000;
 const app = express();
+connectDB();
 
-app.get("/", (req, res) => {
-  res.send("Hello hossein!!!");
-});
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use("/api/products", productRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-  console.log(`Environment variable PORT is set to: ${process.env.PORT}`);
 });
